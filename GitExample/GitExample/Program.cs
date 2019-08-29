87df18f8d886d33a39a7e6d376c2142053104a2c
saikumar15 <saikumar.Vtalent@gmail.com>
2019-08-29 14:11:29 +0530

    Add Branch4

--- GitExample/GitExample/Program.cs
+++ GitExample/GitExample/Program.cs
@@ -10,10 +10,7 @@ namespace GitExample
     {
         static void Main(string[] args)
         {
-            //Branch2 added
-
-            //Branch3 added
-
+            //Branch4 added
         }
     }
 }
